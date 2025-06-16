92b1fc001a7e73f4273406b278923b4a9fd8cf91
INMD1 <lyw514549@gmail.com>
2025-06-16 21:18:25 +0900

    키생성 및 코드 작성

--- routers/mqtt_router.py
+++ routers/mqtt_router.py
@@ -3,6 +3,13 @@ from services.mqtt_service import MQTTService
 
 router = APIRouter(prefix="/mqtt", tags=["mqtt"])
 
+#해당 메서드는 Any 테스트 용도로만 쓰세요.
+#URL: /mqtt/publish
 @router.post("/publish")
-async def publish_message(topic: str, message: str):
-    return await MQTTService.publish_message(topic, message)
+async def publish_message(topic: str, message: str, password: str):
+    return await MQTTService.publish_message(topic, message, password)
+
+
+@router.post("/embed_test")
+async def publish_message(type: int, RSA_PL: str):
+      return await MQTTService.embed(type, RSA_PL)
--- services/mqtt_service.py
+++ services/mqtt_service.py
@@ -1,7 +1,20 @@
 from mqtt_handler import mqtt
+from fastapi import HTTPException
+import time
+import json
 
 class MQTTService:
     @staticmethod
-    async def publish_message(topic, message):
-        mqtt.publish(topic, message)
-        return {"result": True, "message": "Published"}
+    async def publish_message(topic, message, password):
+        if(password == "nullcodoe"):
+            json_data = {
+                "message": message,
+                "time_Arlet":  time.strftime('%c', time.localtime(time.time()))
+            }
+            mqtt.publish(topic, json.dumps(json_data))
+            return {"result": True, "message": "Published"}
+        else:
+          raise HTTPException(status_code=503, detail="Service Unavailable: Invalid password")            
+
+    async def embed(type, RSA_PL):
+        
